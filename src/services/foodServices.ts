bd72f3a8ab4f60bc552212e68ca5555f050a6c7c
alhanampi <alhanampi@gmail.com>
2023-04-02 17:37:38 -0300

    probando chat-gpt para convertir a axios

--- src/services/foodServices.ts
+++ src/services/foodServices.ts
@@ -1,34 +1,44 @@
+import axios from 'axios';
+
 const url1 = `https://api.spoonacular.com`;
 const url2 = `?number=10&apiKey=`;
 
 export const getRandom = async (): Promise<any> => {
-  const api: Response = await fetch(
-    `${url1}/recipes/random${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}`
-  );
-  const data = await api.json();
-  console.log('data random', `${url1}/recipes/random${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}`);
-
-  localStorage.setItem("randomRecipes", JSON.stringify(data.recipes));
-  return data.recipes;
+  try {
+    const response = await axios.get(`${url1}/recipes/random${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}`);
+    
+    const { recipes } = response.data;
+    
+    localStorage.setItem("randomRecipes", JSON.stringify(recipes));
+    
+    return recipes;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch random recipes: ${error}`);
+  }
 };
 
 export const getVeggie = async (): Promise<any> => {
-  const api: Response = await fetch(
-    `${url1}/recipes/random${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}&tags=vegetarian`
-  );
-  const data = await api.json();
-  localStorage.setItem("veggieRecipes", JSON.stringify(data.recipes));
-
-  return data.recipes;
+  try {
+    const response = await axios.get(`${url1}/recipes/random${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}&tags=vegetarian`);
+    
+    const { recipes } = response.data;
+    
+    localStorage.setItem("veggieRecipes", JSON.stringify(recipes));
+    
+    return recipes;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch vegetarian recipes: ${error}`);
+  }
 };
 
 export const getDetails = async (id: string): Promise<any> => {
-  const api: Response = await fetch(
-    `${url1}/recipes/${id}/card?apiKey=${process.env.NEXT_PUBLIC_FOODIE_API_KEY}`
-    );
-    const data = await api.json();
-
-  return data;
+  try {
+    const response = await axios.get(`${url1}/recipes/${id}/card?apiKey=${process.env.NEXT_PUBLIC_FOODIE_API_KEY}`);
+    
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch recipe details: ${error}`);
+  }
 };
 
 // export const jokeOfTheDay = async () => {
@@ -41,9 +51,13 @@ export const getDetails = async (id: string): Promise<any> => {
 // };
 
 export const getCategory = async (type: string): Promise<any> => {
-  const api: Response = await fetch(
-    `${url1}/recipes/complexSearch${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}&cuisine=${type}`
-  );
-  const data = await api.json();
-  return data.results;
+  try {
+    const response = await axios.get(`${url1}/recipes/complexSearch${url2}${process.env.NEXT_PUBLIC_FOODIE_API_KEY}&cuisine=${type}`);
+    
+    const { results } = response.data;
+    
+    return results;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch recipes by category: ${error}`);
+  }
 };
